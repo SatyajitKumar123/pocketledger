dd89e26307d3d9d932496a70ab38522b2532c474
Satyajitkumar123 <satyajitkumar2472@gmail.com>
2025-12-19 17:10:10 +0000

    Added Dashboard, Update, Delete, and Date features

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Transaction
 from .serializers import TransactionSerializer
 
@@ -6,3 +8,8 @@ from .serializers import TransactionSerializer
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all().order_by('-created_at')
     serializer_class = TransactionSerializer
+    
+    @action(detail=False, methods=['delete'])
+    def clear_all(self, request):
+        Transaction.objects.all().delete()
+        return Response({'message': 'History cleared successfully'})
